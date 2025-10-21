97f7893d3cbd06c42120f51d3c864a9b33f52734
Dungndph20697 <dungthoai321@gmail.com>
2025-10-21 14:45:40 +0700

    bt sum Fibonacci

---
+++ bt_sum_Fibonacci/src/index.ts
@@ -0,0 +1,17 @@
+console.log('Happy developing ✨')
+function fibonacci(n: number): number {
+    if (n <= 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+let num = 10; 
+let sum = 0;
+
+console.log("Dãy Fibonacci:");
+for (let i = 0; i < num; i++) {
+    const fib = fibonacci(i);
+    console.log(fib);
+    sum += fib;
+}
+
+console.log(`Tổng của ${num} số Fibonacci đầu tiên là: ${sum}`);
